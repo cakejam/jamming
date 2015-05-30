8ff6ac8c9637c3354e44439b10b6f658e54e6791
smaknp <smaknp@gmail.com>
2015-05-30 18:43:49 -0400

    Added slasher ghost and movement scripts

---
+++ Assets/2dassets/scripts/GhostRun.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostRun : MonoBehaviour {
+
+	private float min=2f;
+	private float max=3f;
+	private float previous;
+	private bool increasing = false;
+
+	// Use this for initialization
+	void Start () {
+		min=transform.position.x;
+		max=transform.position.x+19;
+		previous = min;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xpos = Mathf.PingPong (Time.time * 6, max - min) + min;
+		if ((xpos - previous > 0 && !increasing) || (xpos - previous < 0 && increasing)) {
+			increasing = !increasing;
+			Flip();
+		}
+		previous = xpos;
+		transform.position =new Vector3(xpos, transform.position.y, transform.position.z);
+		
+	}
+
+	void Flip()
+	{
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
