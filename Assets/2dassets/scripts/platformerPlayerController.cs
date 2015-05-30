5a2b4a0cc0f312eb5e496d977320ef55414f6368
Alexander Mistakidis <mistakidis@gmail.com>
2015-05-30 14:07:58 -0400

    added lava, improved player responsiveness, added misc prefabs

--- Assets/2dassets/scripts/platformerPlayerController.cs
+++ Assets/2dassets/scripts/platformerPlayerController.cs
@@ -5,15 +5,18 @@ public class platformerPlayerController : MonoBehaviour {
 	
 	[HideInInspector] public bool facingRight = true;
 	[HideInInspector] public bool jump = false;
+	[HideInInspector] public bool frozen = false;
 	public float moveForce = 365f;
 	public float maxSpeed = 5f;
 	public float jumpForce = 100f;
 	public Transform groundCheck;
-	
+	Vector3 startPoint;
 	
 	private bool grounded = false;
 	private Animator anim;
 	private Rigidbody2D rb2d;
+
+	public GameObject explosionPrefab;
 	
 	
 	// Use this for initialization
@@ -21,45 +24,61 @@ public class platformerPlayerController : MonoBehaviour {
 	{
 		anim = GetComponent<Animator>();
 		rb2d = GetComponent<Rigidbody2D>();
+		startPoint = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () 
-	{
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1);
-
-		if (Input.GetButtonDown("Jump") && grounded)
+	{ 
+		if (Input.GetButtonDown("Jump"))
 		{
-			jump = true;
+			Debug.Log ("jump press");
+			float distanceAboveGround = 5.0f; // When the player is 5 units vertically above ground, hit.collider != null.
+			RaycastHit2D hit;
+			LayerMask layermask = 1 << LayerMask.NameToLayer("Ground"); // Add a layer mask via inspector if you want
+			hit = Physics2D.Raycast(groundCheck.position, -Vector2.up, 0.1f);
+			Debug.DrawLine(groundCheck.position, new Vector3(0, 0, -20.1f), Color.white);
+			if(hit.collider != null) {
+				Debug.Log ("what the " + hit.collider.name);
+				jump = true;
+			}
 		}
 	}
 	
 	void FixedUpdate()
 	{
-		float h = Input.GetAxis("Horizontal");
-		
-		anim.SetFloat("Speed", Mathf.Abs(h));
-		
-		if (h * rb2d.velocity.x < maxSpeed)
-			rb2d.AddForce(Vector2.right * h * moveForce);
-		
-		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
-			rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-		
-		if (h > 0 && !facingRight)
-			Flip ();
-		else if (h < 0 && facingRight)
-			Flip ();
-		
-		if (jump)
-		{
-			Debug.Log ("jump");
-			anim.SetTrigger("Jump");
-			rb2d.AddForce(new Vector2(0f, jumpForce));
-			jump = false;
+		if (!frozen) {
+			float h = Input.GetAxis("Horizontal");
+			
+			anim.SetFloat("Speed", Mathf.Abs(h));
+
+			if (Mathf.Abs(h) > 0) {
+
+				if (h * rb2d.velocity.x < maxSpeed)
+					rb2d.AddForce(Vector2.right * h * moveForce);
+				
+				if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+					rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+				
+				if (h > 0 && !facingRight)
+					Flip ();
+				else if (h < 0 && facingRight)
+					Flip ();
+			}
+			else if (!jump) {
+				rb2d.velocity = new Vector2(0,rb2d.velocity.y);
+
+			}
+			
+			if (jump)
+			{
+				Debug.Log ("jump");
+				anim.SetTrigger("Jump");
+				rb2d.AddForce(new Vector2(0f, jumpForce));
+				jump = false;
+			}
 		}
 	}
-	
 	
 	void Flip()
 	{
@@ -67,5 +86,48 @@ public class platformerPlayerController : MonoBehaviour {
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.layer == LayerMask.NameToLayer("Deadly")) {
+			Debug.Log ("Deadly!");
+			Instantiate(explosionPrefab, this.transform.position, Quaternion.identity);
+			StartCoroutine(WaitAndRestart());         
+		}
+	}
+
+	IEnumerator WaitAndRestart()
+	{
+		Hide ();
+		Freeze();
+		yield return new WaitForSeconds(1);
+		ResetPosition();
+		Show ();
+		UnFreeze();
+	}
+
+	void ResetPosition() {
+		this.transform.position = startPoint;
+		GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		GetComponent<Rigidbody2D>().angularVelocity = 0f;
+		jump = false;
+	}
+
+	void Show() {
+		GetComponent<Renderer>().enabled = true;
+	}
+
+	void Hide() {
+		GetComponent<Renderer>().enabled = false;
+	}
+
+	void Freeze() {
+		GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		GetComponent<Rigidbody2D>().angularVelocity = 0f;
+		frozen = true;
+	}
+
+	void UnFreeze() {
+		frozen = false;
 	}
 }
