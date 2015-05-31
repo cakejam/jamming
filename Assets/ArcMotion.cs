3919fc97fd8dd46c1d1951b3fd15611ace61ad89
Christine <christine.xinyu.li@gmail.com>
2015-05-31 15:05:25 -0400

    Added motion for santa, bunny, arrow shooting

--- Assets/ArcMotion.cs
+++ Assets/ArcMotion.cs
@@ -2,60 +2,22 @@
 using System.Collections;
 
 public class ArcMotion : MonoBehaviour {
+	Vector3 startPosition;
 
-	public Transform Target;
-	public float firingAngle = 45.0f;
-	public float gravity = 9.8f;
-	
-	public Transform Projectile;      
-	private Transform Cupid;
-	
-	void Awake()
-	{  
+	public float springFactor = 8f;
+
+	void Start () {
+		GetComponent<Renderer>().enabled = false;
+		startPosition = this.transform.position;
+		StartCoroutine (Shoot());
 	}
-	
-	void Start()
-	{          
-		Target = GameObject.Find ("player").transform;
-		Cupid = GameObject.Find ("Cupid-shooting").transform;
-		StartCoroutine(SimulateProjectile());
-	}
-	
-	
-	IEnumerator SimulateProjectile()
-	{
-		Projectile = this.transform; 
-		// Short delay added before Projectile is thrown
-		yield return new WaitForSeconds(1.5f);
-		
-		// Move projectile to the position of throwing object + add some offset if needed.
-		Projectile.position = Cupid.position + new Vector3(0, 0.0f, 0);
-		
-		// Calculate distance to target
-		float target_Distance = Vector3.Distance(Projectile.position, Target.position);
-		
-		// Calculate the velocity needed to throw the object to the target at specified angle.
-		float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
-		
-		// Extract the X  Y componenent of the velocity
-		float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
-		float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
-		
-		// Calculate flight time.
-		float flightDuration = target_Distance / Vx;
-		
-		// Rotate projectile to face the target.
-		Projectile.rotation = Quaternion.LookRotation(Target.position - Projectile.position);
-		
-		float elapse_time = 0;
-		
-		while (elapse_time < flightDuration)
-		{
-			Projectile.Translate(0, (Vy - (gravity * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
-			
-			elapse_time += Time.deltaTime;
-			
-			yield return null;
+
+	IEnumerator Shoot(){
+		while (true) {
+			yield return new WaitForSeconds(1.7f);
+			GetComponent<Renderer>().enabled = true;
+			this.transform.position = startPosition;
+			GetComponent<Rigidbody2D> ().velocity = new Vector3 (30, 0, 0);
 		}
-	}  
+	}
 }
