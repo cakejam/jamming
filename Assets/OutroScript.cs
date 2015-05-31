0f2cc6bf2783ca6f287bb476d9faede6813dc367
ameesha <asisaac3@gmail.com>
2015-05-30 21:22:36 -0400

    text for ending scene

--- Assets/OutroScript.cs
+++ Assets/OutroScript.cs
@@ -2,20 +2,20 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class IntroScript : MonoBehaviour {
-
+public class OutroScript : MonoBehaviour {
+	
 	private int w = 550;
 	private int h = 100;
 	private Rect textArea;
 	private GUIStyle style;
 	private Color textColor;
 	private string message;
-	private ArrayList introMessages = new ArrayList();
+	private ArrayList outroMessages = new ArrayList();
 	bool showMessage = true;
 	bool showNextMessage = true;
 	bool showLastMessage = false;
 	int timer = 0;
-
+	
 	// Use this for initialization
 	void Start () {
 		style = new GUIStyle();
@@ -27,30 +27,28 @@ public class IntroScript : MonoBehaviour {
 		style.normal.textColor = textColor;
 		textArea = new Rect(0, 0, Screen.width, Screen.height);
 
-		introMessages.Add("....");
-		introMessages.Add("...Where am I?....");
-		introMessages.Add("I feel like shit.....");
-		introMessages.Add("Is this my house?");
-		introMessages.Add("I need to go inside and lie down");
+		outroMessages.Add("....");
+		outroMessages.Add("...WTF...");
+		outroMessages.Add("I'm never drinking again.");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
-
+	
 	void OnGUI()
 	{
 		if (showMessage){
-			message = (string)introMessages[0];
+			message = (string)outroMessages[0];
 			if (showNextMessage){
-				introMessages.RemoveAt(0);
-				if (introMessages.Count == 0){
+				outroMessages.RemoveAt(0);
+				if (outroMessages.Count == 0){
 					showMessage = false;
 					showLastMessage = true;
-					message = "I need to go inside and lie down";
+					message = "I'm never drinking again.";
 				}
 				else{
-					message = (string)introMessages[0];
+					message = (string)outroMessages[0];
 				}
 			}
 			GUI.Label(textArea, message, style);
@@ -58,14 +56,14 @@ public class IntroScript : MonoBehaviour {
 				StartCoroutine(showMessageTimer());
 				showNextMessage = false;
 			}
-
+			
 		}
 		if (showLastMessage){
-			message = "I need to go inside and lie down";
+			message = "I'm never drinking again.";
 			GUI.Label(textArea, message, style);
 		}
 	}
-
+	
 	IEnumerator showMessageTimer(){
 		yield return new WaitForSeconds(3);
 		Debug.Log ("end");
